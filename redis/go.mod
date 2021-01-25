c436d9130f2cbad9df9fb9acce96923eaf3751f9
lzle <lianzhilei0711@163.com>
2021-01-25 18:25:29 +0800

    redis

---
+++ redis/go.mod
@@ -0,0 +1,8 @@
+module go.lzle.cn/redis
+
+go 1.15
+
+require (
+	github.com/chasex/redis-go-cluster v1.0.0
+	github.com/go-redis/redis/v8 v8.4.10
+)
---
+++ redis/goredis/main.go
@@ -0,0 +1,38 @@
+package main
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+var ctx = context.Background()
+
+
+func main()  {
+	rdb := redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: []string{"192.168.188.105:7000", "192.168.188.105:7001", "192.168.188.105:7002",
+				"192.168.188.110:7000", "192.168.188.110:7001", "192.168.188.110:7002"},
+	})
+	rdb.Ping(ctx)
+
+	err := rdb.Set(ctx, "name", "lzle", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "missing_key").Result()
+	if err == redis.Nil {
+		fmt.Println("missing_key does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("missing_key", val2)
+	}
+
+}
